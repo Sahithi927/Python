f6c8502a658b82c1c6d9e755b4736e582c2b495d
Sahithi927 <140764725+Sahithi927@users.noreply.github.com>
2023-07-31 22:00:40 -0400

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Package',
+    version='1.0.0',
+    description='First',
+    author='Sahithi',
+    author_email='sahithich220@email.com',
+    packages=find_packages(),
+    install_requires=[
+        # Add your project dependencies here
+    ],
